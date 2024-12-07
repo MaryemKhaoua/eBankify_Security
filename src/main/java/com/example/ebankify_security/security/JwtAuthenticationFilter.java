e3934d627c3ae45dea562f9a48ca5a2ca13f9f7f
Maryem khaoua <maryem.khaoua@gmail.com>
2024-12-07 17:41:42 +0100

    i forgot what i did in this commit :)

--- src/main/java/com/example/ebankify_security/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/ebankify_security/security/JwtAuthenticationFilter.java
@@ -6,6 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +21,8 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
 
     @Override
     protected void doFilterInternal(
@@ -23,9 +30,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
          @NonNull HttpServletResponse response,
          @NonNull FilterChain filterChain)
             throws ServletException, IOException {
-         String authHeader = request.getHeader("Authorization");
-         String token;
-         String username;
+        String authHeader = request.getHeader("Authorization");
+        String token;
+        String username;
 
         if (authHeader == null || !authHeader.startsWith("Bearer")) {
             filterChain.doFilter(request, response);
@@ -33,6 +40,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
         token = authHeader.substring(7);
         username = jwtService.extractUsername(token);
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        }
 
     }
 }
--- src/main/java/com/example/ebankify_security/security/JwtService.java
+++ src/main/java/com/example/ebankify_security/security/JwtService.java
@@ -1,10 +1,80 @@
 package com.example.ebankify_security.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HexFormat;
+import java.util.Map;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET = "62e3cc53621e6b9066cbcee3b5d2b607fc23e6ed1d861154206ee7f3d0e74aba";
+
     public String extractUsername(String token) {
-    return  null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+
+    }
+
+    public String generateToken(
+            Map<String, Object > extraClaims,
+            UserDetails userDetails
+    ) {
+        return  Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignKey(), SignatureAlgorithm.ES256)
+                .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final  String username = extractUsername(token);
+       return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+
+
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
